09b1f0755c7b35c623c4626c691bf7d2a3699fd4
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-06-13 19:05:48 -0400

    Added parse for comments

--- src/Comment.hs
+++ src/Comment.hs
@@ -1,9 +1,25 @@
 module Comment where
 
+import Text.Parsec
+import Text.Parsec.String (Parser)
+import Control.Applicative hiding (many, optional)
+import qualified Text.Parsec.Char as PC
+import Text.ParserCombinators.ReadP (choice)
+
 data Comment = CommentLine String
              | CommentBlock String
              deriving Show
 
-data Line = Line
 
-data Block = StartBlock | EndBlock
+parseCommentLine :: Parser Comment
+parseCommentLine = do
+    string "//"
+    value <- manyTill (noneOf "\n") eof
+    return (CommentLine value)
+
+parseCommentBlock :: Parser Comment
+parseCommentBlock = do
+    string "/*"
+    value <- many (noneOf "*")
+    string "*/"
+    return (CommentLine value)
