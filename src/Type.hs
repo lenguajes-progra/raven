d121f4c7430f33f28b1428e938268542c5066e3a
Gabriela Gonzales Villarroel <gabriela.gonzales.60113@usalesiana.edu.bo>
2023-06-12 22:51:13 -0400

    Added Element, Type and VariableDefinition data types

--- src/Type.hs
+++ src/Type.hs
@@ -2,9 +2,25 @@ module Type where
 
 import Text.Parsec
 import Text.Parsec.String (Parser)
+import Literal
 
 data Identifier = Identifier String
-  deriving (Show)
+                deriving (Show)
+
+data Element = LiteralElement Literal
+             | IdentifierElement Identifier
+             deriving (Show)
+
+data Type = IntType
+          | CharType
+          | BooleanType
+          | StringType
+          | FunctionType
+          deriving (Show)
+
+data VariableDefinition = VariableDefinitionType Type Identifier (Maybe Element)
+                        | VariableAssignment Identifier Element
+                        deriving (Show)
 
 identifierParser :: Parser Identifier
 identifierParser = do
@@ -12,6 +28,37 @@ identifierParser = do
   rest <- many (letter <|> digit <|> char '_')
   return (Identifier (first:rest))
 
+elementParser :: Parser Element
+elementParser = choice [LiteralElement <$> literalParser,
+                        IdentifierElement <$> identifierParser]
+
 parseIdentifier :: String -> Either ParseError Identifier
 parseIdentifier input = parse identifierParser "" input
 
+typeParser :: Parser Type
+typeParser = choice [string "int" >> return IntType,
+                     string "char" >> return CharType,
+                     string "boolean" >> return BooleanType,
+                     string "string" >> return StringType,
+                     string "function" >> return FunctionType]
+
+variableDefinitionParser :: Parser VariableDefinition
+variableDefinitionParser = choice [variableDefinitionTypeParser,
+                                   variableAssignmentParser]
+
+variableDefinitionTypeParser :: Parser VariableDefinition
+variableDefinitionTypeParser = do
+  varType <- typeParser
+  spaces
+  varIdentifier <- identifierParser
+  spaces
+  assignment <- optionMaybe (char '=' >> spaces >> elementParser)
+  _ <- char ';'
+  return (VariableDefinitionType varType varIdentifier assignment)
+
+variableAssignmentParser :: Parser VariableDefinition
+variableAssignmentParser = do
+  varIdentifier <- identifierParser
+  spaces
+  assignment <- char '=' >> spaces >> elementParser
+  return (VariableAssignment varIdentifier assignment)
