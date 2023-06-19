c53bd06825b6b6696fae97bf34e4a0830ee09cf7
gabrielagonzalesvillarroel <Gabriela.Gonzales.60113@usalesiana.edu.bo>
2023-06-19 09:47:10 -0400

    Added test for Expressions

--- test/ExpressionTest.hs
+++ test/ExpressionTest.hs
@@ -10,8 +10,23 @@ import Test.Tasty.HUnit
 import Text.Parsec
 import Control.Exception (catch)
 
-testParseExpressionNumericExp :: Assertion
-testParseExpressionNumericExp = assertEqual "parseExpressionNumericExp" (parse parseExpression "" "1 > 2") (Right (NumericExpression (NumericOp (Literal (IntegerLiteral 1)) GreaterThan (Literal (IntegerLiteral 2)))))
+testParseExpressionNumericExpGreater :: Assertion
+testParseExpressionNumericExpGreater = assertEqual "parseExpressionNumericExpGreater" (parse parseExpression "" "1 > 2") (Right (NumericExpression (NumericOp (Literal (IntegerLiteral 1)) GreaterThan (Literal (IntegerLiteral 2)))))
+
+testParseExpressionNumericExpGreaterEq :: Assertion
+testParseExpressionNumericExpGreaterEq = assertEqual "parseExpressionNumericExpGreaterEq" (parse parseExpression "" "1 >= 2") (Right (NumericExpression (NumericOp (Literal (IntegerLiteral 1)) GreatEqualThan (Literal (IntegerLiteral 2)))))
+
+testParseExpressionNumericExpLess :: Assertion
+testParseExpressionNumericExpLess = assertEqual "parseExpressionNumericExpLess" (parse parseExpression "" "1 < 2") (Right (NumericExpression (NumericOp (Literal (IntegerLiteral 1)) LessThan (Literal (IntegerLiteral 2)))))
+
+testParseExpressionNumericExpLessEq :: Assertion
+testParseExpressionNumericExpLessEq = assertEqual "parseExpressionNumericExpLessEq" (parse parseExpression "" "1 <= 2") (Right (NumericExpression (NumericOp (Literal (IntegerLiteral 1)) LessEqualThan (Literal (IntegerLiteral 2)))))
+
+testParseExpressionNumericExpDifferent :: Assertion
+testParseExpressionNumericExpDifferent = assertEqual "parseExpressionNumericExpDifferent" (parse parseExpression "" "1 != 2") (Right (NumericExpression (NumericOp (Literal (IntegerLiteral 1)) NotEqual (Literal (IntegerLiteral 2)))))
+
+testParseExpressionNumericExpEqual :: Assertion
+testParseExpressionNumericExpEqual = assertEqual "parseExpressionNumericExpEqual" (parse parseExpression "" "1 == 2") (Right (NumericExpression (NumericOp (Literal (IntegerLiteral 1)) Equal (Literal (IntegerLiteral 2)))))
 
 testParseExpressionLogicalOrExp :: Assertion
 testParseExpressionLogicalOrExp = assertEqual "parseExpressionLogicalOrExp" (parse parseExpression "" "true || false") (Right (LogicalExpression (LogicOp (Literal (BooleanLiteral True)) Or (Literal (BooleanLiteral False)))))
@@ -22,13 +37,39 @@ testParseExpressionLogicalAndExp = assertEqual "parseExpressionLogicalAndExp" (p
 testParseExpressionLogicalNotExp :: Assertion
 testParseExpressionLogicalNotExp = assertEqual "parseExpressionLogicalNotExp" (parse parseExpression "" "!false") (Right (LogicalExpression (LogicNot (Literal (BooleanLiteral False)))))
 
-testParseExpressionBitExp :: Assertion
-testParseExpressionBitExp = assertEqual "parseExpressionBitExp" (parse parseExpression "" "1 >> 10") (Right (BitExpression (BitOp (Literal (IntegerLiteral 1)) RightShift (Literal (IntegerLiteral 10)))))
+testParseExpressionBitExpAnd :: Assertion
+testParseExpressionBitExpAnd = assertEqual "parseExpressionBitExpAnd" (parse parseExpression "" "1 & 10") (Right (BitExpression (BitOp (Literal (IntegerLiteral 1)) AndBit (Literal (IntegerLiteral 10)))))
+
+testParseExpressionBitExpOr :: Assertion
+testParseExpressionBitExpOr = assertEqual "parseExpressionBitExpOr" (parse parseExpression "" "1 | 10") (Right (BitExpression (BitOp (Literal (IntegerLiteral 1)) OrBit (Literal (IntegerLiteral 10)))))
+
+testParseExpressionBitExpXor :: Assertion
+testParseExpressionBitExpXor = assertEqual "parseExpressionBitExpXor" (parse parseExpression "" "1 ^ 10") (Right (BitExpression (BitOp (Literal (IntegerLiteral 1)) XorBit (Literal (IntegerLiteral 10)))))
+
+testParseExpressionBitExpRightShift :: Assertion
+testParseExpressionBitExpRightShift = assertEqual "parseExpressionBitExpRightShift" (parse parseExpression "" "1 >> 10") (Right (BitExpression (BitOp (Literal (IntegerLiteral 1)) RightShift (Literal (IntegerLiteral 10)))))
+
+testParseExpressionBitExpLeftShift :: Assertion
+testParseExpressionBitExpLeftShift = assertEqual "parseExpressionBitExpLeftShift" (parse parseExpression "" "1 << 10") (Right (BitExpression (BitOp (Literal (IntegerLiteral 1)) LeftShift (Literal (IntegerLiteral 10)))))
+
+testParseExpressionBitExpNotBit :: Assertion
+testParseExpressionBitExpNotBit = assertEqual "parseExpressionBitExpNotBit" (parse parseExpression "" "~10") (Right (BitExpression (BitNot (Literal (IntegerLiteral 10)))))
 
 expressionTests :: TestTree
 expressionTests = testGroup "Builder Expression Tests" [
-    testCase "parseExpressionNumericExp" testParseExpressionNumericExp,
+    testCase "parseExpressionNumericExpGreater" testParseExpressionNumericExpGreater,
+    testCase "parseExpressionNumericExpGreaterEq" testParseExpressionNumericExpGreaterEq,
+    testCase "parseExpressionNumericExpLess" testParseExpressionNumericExpLess,
+    testCase "parseExpressionNumericExpLessEq" testParseExpressionNumericExpLessEq,
+    testCase "parseExpressionNumericExpDifferent" testParseExpressionNumericExpDifferent,
+    testCase "parseExpressionNumericExpEqual" testParseExpressionNumericExpEqual,
     testCase "parseExpressionLogicalOrExp" testParseExpressionLogicalOrExp,
     testCase "parseExpressionLogicalAndExp" testParseExpressionLogicalAndExp,
     testCase "parseExpressionLogicalNotExp" testParseExpressionLogicalNotExp,
-    testCase "parseExpressionBitExp" testParseExpressionBitExp]
+    testCase "parseExpressionBitExpAnd" testParseExpressionBitExpAnd,
+    testCase "parseExpressionBitExpOr" testParseExpressionBitExpOr,
+    testCase "parseExpressionBitExpXor" testParseExpressionBitExpXor,
+    testCase "parseExpressionBitExpRightShift" testParseExpressionBitExpRightShift,
+    testCase "parseExpressionBitExpLeftShift" testParseExpressionBitExpLeftShift,
+    testCase "parseExpressionBitExpNotBit" testParseExpressionBitExpNotBit
+    ]
