bd709cc5769a90fef444399e40b6530047873169
agonzalesb107 <andres.gonzales.60093@usalesiana.edu.bo>
2023-06-20 20:12:32 -0400

    Char printStatement test added

--- test/PrintStatementTest.hs
+++ test/PrintStatementTest.hs
@@ -6,6 +6,7 @@ import PrintStatement
 import Test.Tasty
 import Test.Tasty.HUnit
 import Text.Parsec
+import GHC.IO.Exception (assertError)
 
 testStringWithSpacePrintStatementParser :: Assertion
 testStringWithSpacePrintStatementParser =
@@ -32,12 +33,31 @@ testSpecialCharacterStringSpacePrintStatementParser :: Assertion
 testSpecialCharacterStringSpacePrintStatementParser =
   assertEqual
     "testSpecialCharacterStringSpacePrintStatementParser"
-    (parse printStatementParser "" "print \"Hello$\"")
+    (parse printStatementParser "" "print \"Hello $\"")
     (Right (PrintStatement (Literal (StringLiteral "Hello $"))))
+
+testCharPrintStatementParser :: Assertion
+testCharPrintStatementParser =
+  assertEqual
+    "testCharPrintStatementParser"
+    (parse printStatementParser "" "print 'a'")
+    (Right (PrintStatement (Literal (CharacterLiteral 'a'))))
+
+testNumberCharPrintStatementParser :: Assertion
+testNumberCharPrintStatementParser =
+  assertEqual
+    "testNumberCharPrintStatementParser"
+    (parse printStatementParser "" "print '1'")
+    (Right (PrintStatement (Literal (CharacterLiteral '1'))))
 
 printStatementTest :: TestTree
 printStatementTest =
   testGroup
     "Builder Function Tests"
-    [ testCase "testStringWithSpacePrintStatementParser" testStringWithSpacePrintStatementParser
+    [ testCase "testStringWithSpacePrintStatementParser" testStringWithSpacePrintStatementParser,
+      testCase "testSingleStringPrintStatementParser" testSingleStringPrintStatementParser,
+      testCase "testSpecialCharacterStringPrintStatementParser" testSpecialCharacterStringPrintStatementParser,
+      testCase "testSpecialCharacterStringSpacePrintStatementParser" testSpecialCharacterStringSpacePrintStatementParser,
+      testCase "testCharPrintStatementParser" testCharPrintStatementParser,
+      testCase "testNumberCharPrintStatementParser" testNumberCharPrintStatementParser
     ]
