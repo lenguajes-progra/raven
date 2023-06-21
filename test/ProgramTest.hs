3fa04a4ef045391b6b2e79f4efd248bbe5418a77
agonzalesb107 <andres.gonzales.60093@usalesiana.edu.bo>
2023-06-20 21:32:49 -0400

    Invalid assing value Program test added

--- test/ProgramTest.hs
+++ test/ProgramTest.hs
@@ -27,7 +27,26 @@ testProgramParserAssingCharValue =
   (parse programParser "" "main() `char a = 'a'; char b = 'a'` end")
   (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableDefinitionComplete CharType (Ident "a") (Right (CharacterLiteral 'a'))),VariableDefinition (VariableDefinitionComplete CharType (Ident "b") (Right (CharacterLiteral 'a')))]))))
 
+testProgramParserInvalidAssingIntegerValue :: Assertion
+testProgramParserInvalidAssingIntegerValue =
+  assertEqual
+  "testProgramParserInvalidAssingIntegerValue"
+  (parse programParser "" "main() `int a = a; int b = b` end")
+  (Right (Left (ErrorType Syntax)))
 
+testProgramParserInvalidAssingStringValue :: Assertion
+testProgramParserInvalidAssingStringValue =
+  assertEqual
+  "testProgramParserInvalidAssingStringValue"
+  (parse programParser "" "main() `string a = 1; string b = 2` end")
+  (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableDefinitionComplete StringType (Ident "a") (Left (ErrorType AssignType))),VariableDefinition (VariableDefinitionComplete StringType (Ident "b") (Left (ErrorType AssignType)))]))))
+
+testProgramParserInvalidAssingCharValue :: Assertion
+testProgramParserInvalidAssingCharValue =
+  assertEqual
+  "testProgramParserInvalidAssingCharValue"
+  (parse programParser "" "main() `char a = 1; char b = 2` end")
+  (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableDefinitionComplete CharType (Ident "a") (Left (ErrorType AssignType))),VariableDefinition (VariableDefinitionComplete CharType (Ident "b") (Left (ErrorType AssignType)))]))))
 
 programTest :: TestTree
 programTest =
@@ -35,5 +54,8 @@ programTest =
     "Builder Function Tests"
     [ testCase "testProgramParserAssingIntegerValue" testProgramParserAssingIntegerValue,
       testCase "testProgramParserAssingStringValue" testProgramParserAssingStringValue,
-      testCase "testProgramParserAssingCharValue" testProgramParserAssingCharValue
+      testCase "testProgramParserAssingCharValue" testProgramParserAssingCharValue,
+      testCase "testProgramParserInvalidAssingIntegerValue" testProgramParserInvalidAssingIntegerValue,
+      testCase "testProgramParserInvalidAssingStringValue" testProgramParserInvalidAssingStringValue,
+      testCase "testProgramParserInvalidAssingCharValue" testProgramParserInvalidAssingCharValue
     ]
