6dc93aaa51a37ddd0c5cce9886a919f779d766d8
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-06-28 11:38:46 -0400

    show exp block

--- src/writers/IfTemplate.hs
+++ src/writers/IfTemplate.hs
@@ -1,8 +1,8 @@
 module IfTemplate where
--- import Grammar
+import Grammar
 
-type Expression = String
-type Block = String
+-- type Expression = String
+-- type Block = String
 
 ifTemplate :: Expression -> Block -> Block -> String
-ifTemplate ex bl1 bl2 = "if" ++ " " ++ ex ++ " " ++ "then" ++ " " ++ bl1 ++ " " ++ "else" ++ " " ++ bl2
+ifTemplate ex bl1 bl2 = "if" ++ " " ++ show ex ++ " " ++ "then" ++ " " ++ show bl1 ++ " " ++ "else" ++ " " ++ show bl2
