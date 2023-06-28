6c9348a0bb189bc4c65b243f0f274f5747733cab
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-06-28 09:46:44 -0400

    Updated tests

--- test/ProgramTest.hs
+++ test/ProgramTest.hs
@@ -11,21 +11,21 @@ testProgramParserAssingIntegerValue =
   assertEqual
     "testProgramParserAssingIntegerValue"
     (parse programParser "" "main() `int a = 2; int b = 1` end")
-    (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableDefinitionComplete IntType (Ident "a") (Right (IntegerLiteral 2))), VariableDefinition (VariableDefinitionComplete IntType (Ident "b") (Right (IntegerLiteral 1)))]))))
+    (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableDefinitionComplete IntType (Ident "a") (IntegerLiteral 2)),VariableDefinition (VariableDefinitionComplete IntType (Ident "b") (IntegerLiteral 1))]))))
 
 testProgramParserAssingStringValue :: Assertion
 testProgramParserAssingStringValue = 
   assertEqual
   "testProgramParserAssingStringValue"
   (parse programParser "" "main() `string a = \"hello\"; string b = \"world\"` end")
-  (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableDefinitionComplete StringType (Ident "a") (Right (StringLiteral "hello"))),VariableDefinition (VariableDefinitionComplete StringType (Ident "b") (Right (StringLiteral "world")))]))))
+  (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableDefinitionComplete StringType (Ident "a") (StringLiteral "hello")),VariableDefinition (VariableDefinitionComplete StringType (Ident "b") (StringLiteral "world"))]))))
 
 testProgramParserAssingCharValue :: Assertion
 testProgramParserAssingCharValue =
   assertEqual
   "testProgramParserAssingCharValue"
   (parse programParser "" "main() `char a = 'a'; char b = 'a'` end")
-  (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableDefinitionComplete CharType (Ident "a") (Right (CharacterLiteral 'a'))),VariableDefinition (VariableDefinitionComplete CharType (Ident "b") (Right (CharacterLiteral 'a')))]))))
+  (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableDefinitionComplete CharType (Ident "a") (CharacterLiteral 'a')),VariableDefinition (VariableDefinitionComplete CharType (Ident "b") (CharacterLiteral 'a'))]))))
 
 testProgramParserInvalidAssingIntegerValue :: Assertion
 testProgramParserInvalidAssingIntegerValue =
@@ -39,14 +39,14 @@ testProgramParserInvalidAssingStringValue =
   assertEqual
   "testProgramParserInvalidAssingStringValue"
   (parse programParser "" "main() `string a = 1; string b = 2` end")
-  (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableDefinitionComplete StringType (Ident "a") (Left (ErrorType AssignType))),VariableDefinition (VariableDefinitionComplete StringType (Ident "b") (Left (ErrorType AssignType)))]))))
+  (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableErrorDefinition (ErrorType AssignType)),VariableDefinition (VariableErrorDefinition (ErrorType AssignType))]))))
 
 testProgramParserInvalidAssingCharValue :: Assertion
 testProgramParserInvalidAssingCharValue =
   assertEqual
   "testProgramParserInvalidAssingCharValue"
   (parse programParser "" "main() `char a = 1; char b = 2` end")
-  (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableDefinitionComplete CharType (Ident "a") (Left (ErrorType AssignType))),VariableDefinition (VariableDefinitionComplete CharType (Ident "b") (Left (ErrorType AssignType)))]))))
+  (Right (Right (Program (FuncDefList []) (Block [VariableDefinition (VariableErrorDefinition (ErrorType AssignType)),VariableDefinition (VariableErrorDefinition (ErrorType AssignType))]))))
 
 testIfProgramParser :: Assertion
 testIfProgramParser = 
@@ -60,7 +60,7 @@ testWhileProgramParser =
   assertEqual
   "testWhileProgramParser"
   (parse programParser "" "main() `while (a==b) {int a = 2} end` end")
-  (Right (Right (Program (FuncDefList []) (Block [LoopStat (LoopStatement (NumericExpression (NumericOp (Identifier (Ident "a")) Equal (Identifier (Ident "b")))) (Block [VariableDefinition (VariableDefinitionComplete IntType (Ident "a") (Right (IntegerLiteral 2)))]))]))))
+  (Right (Right (Program (FuncDefList []) (Block [LoopStat (LoopStatement (NumericExpression (NumericOp (Identifier (Ident "a")) Equal (Identifier (Ident "b")))) (Block [VariableDefinition (VariableDefinitionComplete IntType (Ident "a") (IntegerLiteral 2))]))]))))
 
 testEqualExpressionProgramParser :: Assertion
 testEqualExpressionProgramParser =
--- test/TypeTest.hs
+++ test/TypeTest.hs
@@ -10,22 +10,22 @@ import Text.Parsec
 import Control.Exception (catch)
 
 testVariableDefinitionParserWithAssignment :: Assertion
-testVariableDefinitionParserWithAssignment = assertEqual "parseVariableDefinitionWithAssignment" (parse variableDefinitionParser "" "int a = 1;") (Right (VariableDefinitionComplete IntType (Ident "a") (Right (IntegerLiteral 1))))
+testVariableDefinitionParserWithAssignment = assertEqual "parseVariableDefinitionWithAssignment" (parse variableDefinitionParser "" "int a = 1;") (Right (VariableDefinitionComplete IntType (Ident "a") (IntegerLiteral 1)))
 
 testVariableDefinitionParserWithoutAssignment :: Assertion
 testVariableDefinitionParserWithoutAssignment = assertEqual "parseVariableDefinitionWithoutAssignment" (parse variableDefinitionParser "" "int a;") (Right (VariableDefinitionWithoutAssignment IntType (Ident "a")))
 
 testVariableDefinitionParserAssignTypeError :: Assertion
-testVariableDefinitionParserAssignTypeError = assertEqual "parseVariableDefinitionAssignmentError" (parse variableDefinitionParser "" "int a = true;") (Right (VariableDefinitionComplete IntType (Ident "a") (Left (ErrorType AssignType))))
+testVariableDefinitionParserAssignTypeError = assertEqual "parseVariableDefinitionAssignmentError" (parse variableDefinitionParser "" "int a = true;") (Right (VariableErrorDefinition (ErrorType AssignType)))
 
 testArrayDefinitionParserWithAssignment :: Assertion
-testArrayDefinitionParserWithAssignment = assertEqual "parseArrayDefinitionWithAssignment" (parse arrayDefinitionParser "" "[boolean] arr = [true, false];") (Right (ArrayDefinitionComplete (ArrayType BooleanType) (Ident "arr") (Right (Literals [BooleanLiteral True,BooleanLiteral False]))))
+testArrayDefinitionParserWithAssignment = assertEqual "parseArrayDefinitionWithAssignment" (parse arrayDefinitionParser "" "[boolean] arr = [true, false];") (Right (ArrayDefinitionComplete (ArrayType BooleanType) (Ident "arr") (Literals [BooleanLiteral True,BooleanLiteral False])))
 
 testArrayDefinitionParserWithoutAssignment :: Assertion
 testArrayDefinitionParserWithoutAssignment = assertEqual "parseArrayDefinitionWithoutAssignment" (parse arrayDefinitionParser "" "[char] arr;") (Right (ArrayDefinitionWithoutAssignment (ArrayType CharType) (Ident "arr")))
 
 testArrayDefinitionParserAssignTypeError :: Assertion
-testArrayDefinitionParserAssignTypeError = assertEqual "parseArrayDefinitionAssignmentError" (parse arrayDefinitionParser "" "[int] arr = [1,2,true];") (Right (ArrayDefinitionComplete (ArrayType IntType) (Ident "arr") (Left (ErrorType AssignType))))
+testArrayDefinitionParserAssignTypeError = assertEqual "parseArrayDefinitionAssignmentError" (parse arrayDefinitionParser "" "[int] arr = [1,2,true];") (Right (ArrayErrorDefinition (ErrorType AssignType)))
 
 typeTests :: TestTree
 typeTests = testGroup "Builder Type Tests" [
