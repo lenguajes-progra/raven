283b1484e2c1a366e194bf1c112aacde4d0895e6
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-06-29 00:07:24 -0400

    Expression parsed verifying if the operation (logical, bit, or boolean) is valid

--- src/writers/ExpressionTemplate.hs
+++ src/writers/ExpressionTemplate.hs
@@ -6,10 +6,21 @@ import LiteralTemplate (literalTemplate)
 import Parsers
 import VariableDefTemplate (identifierTransformer)
 
+termBitAndBoolTransformer :: Expression -> String
+termBitAndBoolTransformer (Literal (IntegerLiteral il)) = show il
+termBitAndBoolTransformer (Literal _) = throwBitError
+termBitAndBoolTransformer (Identifier iden) = identifierTransformer iden
+termBitAndBoolTransformer (BitExpression be) = bitExpressionTransformer be
+termBitAndBoolTransformer (Parens (BitExpression be)) = "(" ++ bitExpressionTransformer be ++ ")"
+termBitAndBoolTransformer (Parens (Literal (IntegerLiteral il))) = "(" ++ show il ++ ")"
+termBitAndBoolTransformer (Parens (Literal _)) = throwBitError
+termBitAndBoolTransformer _ = throwBitError
+
+-- ! Boolean Expression
 boolOpTransformer :: BooleanOperator -> String
 boolOpTransformer s = case s of
   Equal -> "=="
-  NotEqual -> "!="
+  NotEqual -> "/="
   LessThan -> "<"
   GreaterThan -> ">"
   LessEqualThan -> "<="
@@ -17,21 +28,28 @@ boolOpTransformer s = case s of
 
 boolExpressionTransformer :: BooleanExpression -> String
 boolExpressionTransformer (BooleanOp leftExpr operation rightExpr) =
-  termTransformer leftExpr ++ boolOpTransformer operation ++ expressionTransformer rightExpr
+  termBitAndBoolTransformer leftExpr ++ boolOpTransformer operation ++ termBitAndBoolTransformer rightExpr
+
+-- ! Bit Expression
 
 bitOpTransformer :: BitOperator -> String
 bitOpTransformer s = case s of
-  AndBit -> "&"
-  OrBit -> "|"
+  AndBit -> ".&."
+  OrBit -> ".|."
   XorBit -> "^"
-  LeftShift -> "<<"
-  RightShift -> ">>"
-  NotBit -> "~"
+  LeftShift -> "`shiftL`"
+  RightShift -> "`shiftR`"
+  NotBit -> "complement "
 
 bitExpressionTransformer :: BitExpression -> String
-bitExpressionTransformer (BitNot expr) = bitOpTransformer NotBit ++ termTransformer expr
+bitExpressionTransformer (BitNot expr) = "(" ++ bitOpTransformer NotBit ++ termBitAndBoolTransformer expr ++ ")"
 bitExpressionTransformer (BitOp leftExpr bitOperator rightExpr) =
-  termTransformer leftExpr ++ bitOpTransformer bitOperator ++ expressionTransformer rightExpr
+  termBitAndBoolTransformer leftExpr ++ bitOpTransformer bitOperator ++ termBitAndBoolTransformer rightExpr
+
+throwBitError :: a
+throwBitError = error "Only operation with number allowed"
+
+-- ! Logic Expression
 
 logicOpTransformer :: LogicalOperator -> String
 logicOpTransformer s = case s of
@@ -42,7 +60,24 @@ logicOpTransformer s = case s of
 logicExpressionTransformer :: LogicalExpression -> String
 logicExpressionTransformer (LogicNot expr) = logicOpTransformer Not ++ termTransformer expr
 logicExpressionTransformer (LogicOp leftExpr logicOp rightExpr) =
-  termTransformer leftExpr ++ logicOpTransformer logicOp ++ expressionTransformer rightExpr
+  termLogicTransformer leftExpr ++ logicOpTransformer logicOp ++ termLogicTransformer rightExpr
+
+throwLogicError :: a
+throwLogicError = error "only logical operations are allowed"
+
+termLogicTransformer :: Expression -> String
+termLogicTransformer (Literal (BooleanLiteral bl)) = show bl
+termLogicTransformer (Literal _) = throwLogicError
+termLogicTransformer (Identifier iden) = identifierTransformer iden
+termLogicTransformer (BooleanExpression be) = boolExpressionTransformer be
+termLogicTransformer (LogicalExpression le) = logicExpressionTransformer le
+termLogicTransformer (Parens (BooleanExpression be)) = "(" ++ boolExpressionTransformer be ++ ")"
+termLogicTransformer (Parens (LogicalExpression le)) = "(" ++ logicExpressionTransformer le ++ ")"
+termLogicTransformer (Parens (Literal (BooleanLiteral bl))) = "(" ++ show bl ++ ")"
+termLogicTransformer (Parens (Literal _)) = throwLogicError
+termLogicTransformer _ = throwLogicError
+
+-- ! Expression
 
 expressionTransformer :: Expression -> String
 expressionTransformer (BooleanExpression be) = boolExpressionTransformer be
@@ -54,3 +89,4 @@ termTransformer :: Expression -> String
 termTransformer (Literal lit) = literalTemplate lit
 termTransformer (Identifier iden) = identifierTransformer iden
 termTransformer (Parens expr) = "(" ++ expressionTransformer expr ++ ")"
+termTransformer _ = throwBitError
