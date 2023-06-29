8241b9b86ce41b7cef8c12963b5147142948bc33
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-06-28 21:59:04 -0400

    Expression transformer applied recursively

--- src/writers/ExpressionTemplate.hs
+++ src/writers/ExpressionTemplate.hs
@@ -1,27 +1,56 @@
 module ExpressionTemplate where
+
+import Expression
 import Grammar
+import LiteralTemplate (literalTemplate)
+import Parsers
+import VariableDefTemplate (identifierTransformer)
 
 boolOpTransformer :: BooleanOperator -> String
-boolOpTransformer symbol = case symbol of
-    Equal -> "=="
-    NotEqual -> "!="
-    LessThan -> "<"
-    GreaterThan -> ">"
-    LessEqualThan -> "<="
-    GreatEqualThan -> ">="
+boolOpTransformer s = case s of
+  Equal -> "=="
+  NotEqual -> "!="
+  LessThan -> "<"
+  GreaterThan -> ">"
+  LessEqualThan -> "<="
+  GreatEqualThan -> ">="
+
+boolExpressionTransformer :: BooleanExpression -> String
+boolExpressionTransformer (BooleanOp leftExpr operation rightExpr) =
+  termTransformer leftExpr ++ boolOpTransformer operation ++ expressionTransformer rightExpr
 
 bitOpTransformer :: BitOperator -> String
-bitOpTransformer symbol = case symbol of
-    AndBit -> "&"
-    OrBit -> "|"
-    XorBit -> "^"
-    LeftShift -> "<<"
-    RightShift -> ">>"
-    NotBit -> "~"
+bitOpTransformer s = case s of
+  AndBit -> "&"
+  OrBit -> "|"
+  XorBit -> "^"
+  LeftShift -> "<<"
+  RightShift -> ">>"
+  NotBit -> "~"
+
+bitExpressionTransformer :: BitExpression -> String
+bitExpressionTransformer (BitNot expr) = bitOpTransformer NotBit ++ termTransformer expr
+bitExpressionTransformer (BitOp leftExpr bitOperator rightExpr) =
+  termTransformer leftExpr ++ bitOpTransformer bitOperator ++ expressionTransformer rightExpr
 
 logicOpTransformer :: LogicalOperator -> String
-logicOpTransformer symbol = case symbol of
-    And -> "&&"
-    Or -> "||"
-    Not -> "!"
+logicOpTransformer s = case s of
+  And -> "&&"
+  Or -> "||"
+  Not -> "!"
 
+logicExpressionTransformer :: LogicalExpression -> String
+logicExpressionTransformer (LogicNot expr) = logicOpTransformer Not ++ termTransformer expr
+logicExpressionTransformer (LogicOp leftExpr logicOp rightExpr) =
+  termTransformer leftExpr ++ logicOpTransformer logicOp ++ expressionTransformer rightExpr
+
+expressionTransformer :: Expression -> String
+expressionTransformer (BooleanExpression be) = boolExpressionTransformer be
+expressionTransformer (LogicalExpression le) = logicExpressionTransformer le
+expressionTransformer (BitExpression be) = bitExpressionTransformer be
+expressionTransformer expr = termTransformer expr
+
+termTransformer :: Expression -> String
+termTransformer (Literal lit) = literalTemplate lit
+termTransformer (Identifier iden) = identifierTransformer iden
+termTransformer (Parens expr) = "(" ++ expressionTransformer expr ++ ")"
