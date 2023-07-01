8267ddb39c28ea1511b0b78ea390e1204687736b
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-06-30 21:33:06 -0400

    Statement transformer created

--- src/writers/PrintStatementTemplate.hs
+++ src/writers/PrintStatementTemplate.hs
@@ -1,10 +1,10 @@
 module PrintStatementTemplate where
 
-import Grammar
 import ExpressionTemplate
+import Grammar
 
 printStatementTemplate :: String -> String
 printStatementTemplate exp = "main :: IO()" ++ "\n" ++ "main = print" ++ "(" ++ exp ++ ")"
 
-printStatementParserTransformer :: PrintStatement -> String
-printStatementParserTransformer (PrintStatement exp) = expressionTransformer exp
+printStatementTransformer :: PrintStatement -> String
+printStatementTransformer (PrintStatement exp) = printStatementTemplate (expressionTransformer exp)
