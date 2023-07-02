4e050fd39d6e9442c67f26844fa1f3c650f71e2b
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-07-02 08:58:01 -0400

    Template for ForStatements done

--- src/writers/LoopTemplate.hs
+++ src/writers/LoopTemplate.hs
@@ -2,6 +2,7 @@ module LoopTemplate where
 
 import Grammar
 import ExpressionTemplate
+import System.IO
 
 loopDefTemplate :: String
 loopDefTemplate = "for :: a -> (a -> Bool) -> (a -> a) -> a" ++ "\n" ++ loopBodyTemplate
@@ -10,10 +11,23 @@ loopBodyTemplate :: String
 loopBodyTemplate = "for value condition action = if condition value then for (action value) condition action else value"
 
 loopTransformer :: ForStatement -> String
-loopTransformer (ForStatement value condition action) = "(for " ++ expressionTransformer value ++ " ("
-  ++ expressionTransformer condition ++ ") (" ++ expressionTransformer action ++ "))"
+loopTransformer (ForStatement value condition action) = "(for " ++ (expressionTransformer value)
+  ++ createLamba (expressionTransformer value) (expressionTransformer condition) 
+  ++ createLamba (expressionTransformer value) (expressionTransformer action) ++ ")"
 
 for :: a -> (a -> Bool) -> (a -> a) -> a
 for value condition action = if condition value then for (action value) condition action else value
+
+createLamba :: String -> String -> String
+createLamba value expr = " (\\x -> " ++ replaceSequence value "x" expr ++ ")"
+
+replaceSequence :: String -> String -> String -> String
+replaceSequence sequence replacement expr = 
+    case expr of
+        [] -> []
+        x:xs ->
+            if take (length sequence) expr == sequence
+                then replacement ++ replaceSequence sequence replacement (drop (length sequence) expr)
+                else x : replaceSequence sequence replacement xs
 
 
