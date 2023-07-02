e7466caca77d548d08b00c69bbdb5e2e5769730a
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-07-02 14:56:46 -0400

    If transfoerm created

--- src/writers/IfTemplate.hs
+++ src/writers/IfTemplate.hs
@@ -1,6 +1,10 @@
 module IfTemplate where
-import Grammar
+
 import ExpressionTemplate
+import Grammar
 
 ifTemplate :: Expression -> Expression -> Expression -> String
 ifTemplate cond exp1 exp2 = "if" ++ " " ++ expressionTransformer cond ++ " " ++ "then" ++ " " ++ expressionTransformer exp1 ++ " " ++ "else" ++ " " ++ expressionTransformer exp2
+
+ifTransformer :: IfStatement -> String
+ifTransformer (IfStatement expr1 expr2 expr3) = ifTemplate expr1 expr2 expr3
