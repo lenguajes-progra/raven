4a34dd7c81f6c9f8d9285c05dc35d84361bbb0dc
gabrielagonzalesvillarroel <Gabriela.Gonzales.60113@usalesiana.edu.bo>
2023-07-02 11:28:39 -0400

    Added literal and identifier in expression parser and fix name in loopTemplate

--- src/writers/LoopTemplate.hs
+++ src/writers/LoopTemplate.hs
@@ -2,7 +2,6 @@ module LoopTemplate where
 
 import Grammar
 import ExpressionTemplate
-import System.IO
 
 loopDefTemplate :: String
 loopDefTemplate = "for :: a -> (a -> Bool) -> (a -> a) -> a" ++ "\n" ++ loopBodyTemplate
@@ -11,15 +10,15 @@ loopBodyTemplate :: String
 loopBodyTemplate = "for value condition action = if condition value then for (action value) condition action else value"
 
 loopTransformer :: ForStatement -> String
-loopTransformer (ForStatement value condition action) = "(for " ++ (expressionTransformer value)
-  ++ createLamba (expressionTransformer value) (expressionTransformer condition) 
-  ++ createLamba (expressionTransformer value) (expressionTransformer action) ++ ")"
+loopTransformer (ForStatement value condition action) = "(for " ++ expressionTransformer value
+  ++ createLambda (expressionTransformer value) (expressionTransformer condition) 
+  ++ createLambda (expressionTransformer value) (expressionTransformer action) ++ ")"
 
 for :: a -> (a -> Bool) -> (a -> a) -> a
 for value condition action = if condition value then for (action value) condition action else value
 
-createLamba :: String -> String -> String
-createLamba value expr = " (\\x -> " ++ replaceSequence value "x" expr ++ ")"
+createLambda :: String -> String -> String
+createLambda value expr = " (\\x -> " ++ replaceSequence value "x" expr ++ ")"
 
 replaceSequence :: String -> String -> String -> String
 replaceSequence sequence replacement expr = 
