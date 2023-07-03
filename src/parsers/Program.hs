95bbc35137c0d9bdb3b78ed86856edc5af52b1cc
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-07-03 03:06:31 -0400

    Added handling errors for Program, function list and block

--- src/parsers/Program.hs
+++ src/parsers/Program.hs
@@ -7,13 +7,30 @@ import Text.Parsec
 import Text.Parsec.String
 
 functionsDefinitionParser :: Parser FunctionDefinitionList
-functionsDefinitionParser = FuncDefList <$> functionDefinitionParser `sepBy` (char '\n' *> spaces)
+functionsDefinitionParser = functionDefinitionParser `sepBy` (char '\n' *> spaces) >>= \functions ->
+  return (analyzeFunctions functions)
+
+analyzeFunctions :: [FunctionDefinition] -> FunctionDefinitionList
+analyzeFunctions functions = 
+  if all analyzeFunction functions 
+    then FuncDefList functions 
+  else FuncDefListError (ErrorType Type)
+
+analyzeFunction :: FunctionDefinition -> Bool
+analyzeFunction fc = case fc of
+  (FuncDefinitionError _) -> False
+  _ -> True
 
 programParser :: Parser (Either Error Program)
 programParser =
-  try
-    (Right <$> (Program <$> functionsDefinitionParser))
+  try analyzeFunctionList
     <|> pure (Left (ErrorType Syntax))
+
+analyzeFunctionList :: Parser (Either Error Program)
+analyzeFunctionList = functionsDefinitionParser >>= \functions ->
+  case functions of
+    (FuncDefList _) -> return (Right (Program functions))
+    (FuncDefListError err) -> return (Left err)
 
 textReader :: IO ()
 textReader = do
