e91fdf6c6d5e5dc967de741af54e6dd65b278b27
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-07-03 23:54:57 -0400

    var temp test impl

---
+++ test/VariableTemplateTest.hs
@@ -0,0 +1,48 @@
+module VariableTemplateTest where
+
+import VariableDefTemplate
+import Grammar
+import DataTransformer
+import Test.Tasty
+import Test.Tasty.HUnit (Assertion, assertEqual, testCase)
+import Text.Parsec (Consumed(Empty))
+
+testVariableDefinitionTemplate :: Assertion
+testVariableDefinitionTemplate = assertEqual "variableDefinitionTemplate" (variableDefinitionTemplate "var" "Int" "var * 2") "Int :: var\nInt = var * 2"
+
+testVariableBodyTemplate :: Assertion
+testVariableBodyTemplate = assertEqual "variableBodyTemplate" (variableBodyTemplate "var" "var * 2") "var = var * 2"
+
+testVariableWithoutAssignment :: Assertion
+testVariableWithoutAssignment = assertEqual "variableWithoutAssignment" (variableWithoutAssignment "var") "\twhere var = "
+
+testVariableDefBlockTemplate :: Assertion
+testVariableDefBlockTemplate = assertEqual "variableDefBlockTemplate" (variableDefBlockTemplate "var" "var * 2") "\twhere var = var * 2"
+
+testVariableExpressionTemplate :: Assertion
+testVariableExpressionTemplate = assertEqual "variableExpressionTemplate" (variableExpressionTemplate "var * 2") "var * 2"
+
+testVariableDefinitionTransformerComplete :: Assertion
+testVariableDefinitionTransformerComplete = assertEqual "variableDefinitionTransformerComplete" (variableDefinitionTransformer (VariableDefinitionComplete IntType (Ident "ab") (Literal (IntegerLiteral 2)))) (TriNode "Int" "ab" "2")
+
+testVariableDefinitionTransformerWith :: Assertion
+testVariableDefinitionTransformerWith = assertEqual "variableDefinitionTransformerWith" (variableDefinitionTransformer (VariableDefinitionWithoutAssignment IntType (Ident "result"))) (TwiceNodeWithoutAssignment "Int" "result")
+
+variableDefinitionTemplateTests :: TestTree
+variableDefinitionTemplateTests =
+  testGroup
+    "Builder VariableDefinitionTemplate Tests"
+    [ 
+      testCase "variableDefinitionTemplate" testVariableDefinitionTemplate,
+      testCase "variableBodyTemplate" testVariableBodyTemplate,
+      testCase "variableWithoutAssignment" testVariableWithoutAssignment,
+      testCase "variableDefBlockTemplate" testVariableDefBlockTemplate,
+      testCase "variableExpressionTemplate" testVariableExpressionTemplate,
+      testCase "variableDefinitionTransformerComplete" testVariableDefinitionTransformerComplete,
+      testCase "variableDefinitionTransformerWith" testVariableDefinitionTransformerWith,
+      testCase "variableDefinitionTemplate" testVariableDefinitionTemplate,
+      testCase "variableDefinitionTemplate" testVariableDefinitionTemplate,
+      testCase "variableDefinitionTemplate" testVariableDefinitionTemplate,
+      testCase "variableDefinitionTemplate" testVariableDefinitionTemplate,
+      testCase "variableDefinitionTemplate" testVariableDefinitionTemplate
+    ]
